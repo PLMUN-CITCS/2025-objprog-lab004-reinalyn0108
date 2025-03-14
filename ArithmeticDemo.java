24ce8bf3026ab4e9d4f79f34fc8e77736afeccfc
reinalyn0108 <robetereinalyn_bscs@plmun.edu.ph>
2025-03-14 15:34:28 +0800

    Submitting OBJPROG Week 02 - Session 01 - Exercise 03

---
+++ ArithmeticDemo.java
@@ -0,0 +1,20 @@
+public class ArithmeticDemo {
+    public static void main(String[] args) {
+int num1 = 10;
+int num2 = 5;
+double num3 = 3.0;
+
+int sum = num1 + num2;
+System.out.println("Sum: " + sum);
+int difference = num1 - num2;
+System.out.println("Difference: " + difference);
+int product = num1 *  num2;
+System.out.println("Product: " + product);
+int quotient = num1 / num2;
+System.out.println("Quotient: " + quotient);
+int remainder = num1 % num2;
+System.out.println("Remainder: " + remainder);
+int expressionResult = num1 + num2 * 2 - (num1 / num2);
+System.out.println("Expression Result: " + expressionResult); 
+    }
+}
